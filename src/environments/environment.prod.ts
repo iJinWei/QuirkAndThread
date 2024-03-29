20863f3469fef9dca0a57e3c82b3760cf5c1d770
Nic <jinweinic95@gmail.com>
2024-03-29 18:44:59 +0800

    Updated config for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -16,146 +16,155 @@ export const environment = {
   appPreviewDocsUrl: 'https://preview.keenthemes.com/metronic8/angular/docs',
   appPreviewChangelogUrl: 'https://preview.keenthemes.com/metronic8/angular/docs/changelog',
   appDemos: {
-    'demo1': {
-      'title': 'Demo 1',
-      'description': 'Default Dashboard',
-      'published': true,
-      'thumbnail': './assets/media/demos/demo1.png'
+    demo1: {
+      title: 'Demo 1',
+      description: 'Default Dashboard',
+      published: true,
+      thumbnail: './assets/media/demos/demo1.png',
     },
 
-    'demo2': {
-      'title': 'Demo 2',
-      'description': 'SaaS Application',
-      'published': true,
-      'thumbnail': './assets/media/demos/demo2.png'
+    demo2: {
+      title: 'Demo 2',
+      description: 'SaaS Application',
+      published: true,
+      thumbnail: './assets/media/demos/demo2.png',
     },
 
-    'demo3': {
-      'title': 'Demo 3',
-      'description': 'New Trend',
-      'published': true,
-      'thumbnail': './assets/media/demos/demo3.png'
+    demo3: {
+      title: 'Demo 3',
+      description: 'New Trend',
+      published: true,
+      thumbnail: './assets/media/demos/demo3.png',
     },
 
-    'demo4': {
-      'title': 'Demo 4',
-      'description': 'Intranet Application',
-      'published': true,
-      'thumbnail': './assets/media/demos/demo4.png'
+    demo4: {
+      title: 'Demo 4',
+      description: 'Intranet Application',
+      published: true,
+      thumbnail: './assets/media/demos/demo4.png',
     },
 
-    'demo5': {
-      'title': 'Demo 5',
-      'description': 'Support Forum',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo5.png'
+    demo5: {
+      title: 'Demo 5',
+      description: 'Support Forum',
+      published: false,
+      thumbnail: './assets/media/demos/demo5.png',
     },
 
-    'demo6': {
-      'title': 'Demo 6',
-      'description': 'Admin Backend',
-      'published': true,
-      'thumbnail': './assets/media/demos/demo6.png'
+    demo6: {
+      title: 'Demo 6',
+      description: 'Admin Backend',
+      published: true,
+      thumbnail: './assets/media/demos/demo6.png',
     },
 
-    'demo7': {
-      'title': 'Demo 7',
-      'description': 'CRM Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo7.png'
+    demo7: {
+      title: 'Demo 7',
+      description: 'CRM Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo7.png',
     },
 
-    'demo8': {
-      'title': 'Demo 8',
-      'description': 'Core Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo8.png'
+    demo8: {
+      title: 'Demo 8',
+      description: 'Core Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo8.png',
     },
 
-    'demo9': {
-      'title': 'Demo 9',
-      'description': 'Fancy Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo9.png'
+    demo9: {
+      title: 'Demo 9',
+      description: 'Fancy Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo9.png',
     },
 
-    'demo10': {
-      'title': 'Demo 10',
-      'description': 'Modern Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo10.png'
+    demo10: {
+      title: 'Demo 10',
+      description: 'Modern Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo10.png',
     },
 
-    'demo11': {
-      'title': 'Demo 11',
-      'description': 'Light Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo11.png'
+    demo11: {
+      title: 'Demo 11',
+      description: 'Light Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo11.png',
     },
 
-    'demo12': {
-      'title': 'Demo 12',
-      'description': 'Reporting System',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo12.png'
+    demo12: {
+      title: 'Demo 12',
+      description: 'Reporting System',
+      published: false,
+      thumbnail: './assets/media/demos/demo12.png',
     },
 
-    'demo13': {
-      'title': 'Demo 13',
-      'description': 'Classic Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo13.png'
+    demo13: {
+      title: 'Demo 13',
+      description: 'Classic Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo13.png',
     },
 
-    'demo14': {
-      'title': 'Demo 14',
-      'description': 'Creative Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo14.png'
+    demo14: {
+      title: 'Demo 14',
+      description: 'Creative Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo14.png',
     },
 
-    'demo15': {
-      'title': 'Demo 15',
-      'description': 'Minimalistic Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo15.png'
+    demo15: {
+      title: 'Demo 15',
+      description: 'Minimalistic Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo15.png',
     },
 
-    'demo16': {
-      'title': 'Demo 16',
-      'description': 'Modern Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo16.png'
+    demo16: {
+      title: 'Demo 16',
+      description: 'Modern Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo16.png',
     },
 
-    'demo17': {
-      'title': 'Demo 17',
-      'description': 'Backend System',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo17.png'
+    demo17: {
+      title: 'Demo 17',
+      description: 'Backend System',
+      published: false,
+      thumbnail: './assets/media/demos/demo17.png',
     },
 
-    'demo18': {
-      'title': 'Demo 18',
-      'description': 'System Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo18.png'
+    demo18: {
+      title: 'Demo 18',
+      description: 'System Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo18.png',
     },
 
-    'demo19': {
-      'title': 'Demo 19',
-      'description': 'Light Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo19.png'
+    demo19: {
+      title: 'Demo 19',
+      description: 'Light Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo19.png',
     },
 
-    'demo20': {
-      'title': 'Demo 20',
-      'description': 'Monochrome Dashboard',
-      'published': false,
-      'thumbnail': './assets/media/demos/demo20.png'
+    demo20: {
+      title: 'Demo 20',
+      description: 'Monochrome Dashboard',
+      published: false,
+      thumbnail: './assets/media/demos/demo20.png',
     },
   },
+  firebase: {
+    apiKey: "AIzaSyD7AloVSCSdcaZMIMFi7o3ZuJGu48oEYeM",
+    authDomain: "quirkandthread-a151e.firebaseapp.com",
+    projectId: "quirkandthread-a151e",
+    storageBucket: "quirkandthread-a151e.appspot.com",
+    messagingSenderId: "437738972765",
+    appId: "1:437738972765:web:bddc95ac17587c33098fb4",
+    measurementId: "G-GHPGH1HG1F",
+  }
 };
 
 /*
