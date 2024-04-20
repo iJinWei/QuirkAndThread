69b0e72f56e07c3817aeed4d12bbc25a44288462
gracefour11 <76483230+gracefour11@users.noreply.github.com>
2024-04-20 12:32:10 +0800

    recaptcha

--- src/app/modules/auth/components/registration/registration.component.ts
+++ src/app/modules/auth/components/registration/registration.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
-import { Subscription, Observable } from 'rxjs';
+import { Subscription, Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { ConfirmPasswordValidator, strongPasswordValidator } from './confirm-password.validator';
 import { UserModel } from '../../models/user.model';
-import { first } from 'rxjs/operators';
+import { concatMap, first, switchMap } from 'rxjs/operators';
 import { SharedService } from 'src/app/shared.service';
 import { ReCaptcha2Component } from 'ngx-captcha';
 import { environment } from 'src/environments/environment';
@@ -23,7 +23,6 @@ export class RegistrationComponent implements OnInit, OnDestroy {
 
   siteKey: string;
   @ViewChild('captchaElem') captchaElem: ReCaptcha2Component;
-  @ViewChild('langInput') langInput: ElementRef;
 
   public theme : 'light'|'dark' = 'light'
   public size : 'compact'|'normal' = 'normal'
@@ -35,7 +34,7 @@ export class RegistrationComponent implements OnInit, OnDestroy {
   private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
   recaptchaError: string | null = null;
   isSubmitting: boolean = false;
-  recaptchaResponse: string = '';
+  recaptchaToken: string = '';
 
   constructor(
     private fb: FormBuilder,
@@ -96,12 +95,13 @@ export class RegistrationComponent implements OnInit, OnDestroy {
           ]),
         ],
         agree: [false, Validators.compose([Validators.required])],
-        recaptcha: ['']
+        recaptcha: ['', Validators.required]
       },
       {
         validator: ConfirmPasswordValidator.MatchPassword,
       }
     );
+   
   }
 
   // submit() {
@@ -164,63 +164,101 @@ export class RegistrationComponent implements OnInit, OnDestroy {
   // }
 
   submit() {
-    const token = this.registrationForm?.get('recaptcha')?.value;
-    console.log("token:", typeof token)
-    console.log("token:", token)
-    this.verifyRecaptchaToken(token);
-    /*
-    this.hasError = false;
-    const result: { [key: string]: string } = {};
-    Object.keys(this.f).forEach((key) => {
-      result[key] = this.f[key].value;
-    });
-    const newUser = new UserModel();
-    newUser.setUser(result);
-  
-    const registrationSubscr = this.authService
-      .registrationFirebase(newUser)
-      .subscribe({
-        next: (firebaseUser) => { // Assuming firebaseUser now includes the full user object
-          if (firebaseUser) {
-            // Registration successful, now add user details to Firestore
-            const userData = {
-              email: newUser.email, // Assuming UserModel has an email field
-              name: newUser.fullname, // Assuming UserModel has a name field
-              joinDate: new Date(), // Use current date for joinDate
-              lastLogin: new Date(), // Consider updating this field upon each login
-              roles: ["customer"], // Default role
-            };
-  
-            // Add user data to Firestore (assumes SharedService or similar is injected as sharedService)
-            this.service.addUser(firebaseUser.uid, userData).then(() => {
-              //this.router.navigate(['/']);
-              this.authService.sendEmailVerification(firebaseUser)
-            }).catch((firestoreError: any) => {
-              console.error(firestoreError);
+    this.verifyCaptchaToken().then(valid => {
+      if (valid) {
+        this.hasError = false;
+        const result: { [key: string]: string } = {};
+        Object.keys(this.f).forEach((key) => {
+          result[key] = this.f[key].value;
+        });
+        const newUser = new UserModel();
+        newUser.setUser(result);
+      
+        const registrationSubscr = this.authService
+          .registrationFirebase(newUser)
+          .subscribe({
+            next: (firebaseUser) => { // Assuming firebaseUser now includes the full user object
+              if (firebaseUser) {
+                // Registration successful, now add user details to Firestore
+                const userData = {
+                  email: newUser.email, // Assuming UserModel has an email field
+                  name: newUser.fullname, // Assuming UserModel has a name field
+                  joinDate: new Date(), // Use current date for joinDate
+                  lastLogin: new Date(), // Consider updating this field upon each login
+                  roles: ["customer"], // Default role
+                };
+                console.log("userData: ", userData)
+                console.log("firebaseUser.uid: ", firebaseUser.uid)
+                // Add user data to Firestore (assumes SharedService or similar is injected as sharedService)
+                this.service.addUser(firebaseUser.uid, userData).then(() => {
+                  console.log("adding user")
+                  this.authService.sendEmailVerification(firebaseUser)
+                }).catch((firestoreError: any) => {
+                  console.error(firestoreError);
+                  this.hasError = true;
+                  // Handle Firestore error (e.g., display an error message)
+                  alert('Registration was successful, but there was an error storing additional user details.');
+                });
+              } else {
+                // Handle the unlikely case that registration succeeded without a user object
+                this.hasError = true;
+                alert('Registration failed. Please try again.');
+              }
+            },
+            error: (err) => {
               this.hasError = true;
-              // Handle Firestore error (e.g., display an error message)
-              alert('Registration was successful, but there was an error storing additional user details.');
-            });
-          } else {
-            // Handle the unlikely case that registration succeeded without a user object
-            this.hasError = true;
-            alert('Registration failed. Please try again.');
-          }
-        },
-        error: (err) => {
-          this.hasError = true;
-          console.error(err);
-          alert(`Registration failed: ${err.message}`);
-        }
-      });
-  
-    this.unsubscribe.push(registrationSubscr);
-    */
+              console.error(err);
+              alert(`Registration failed: ${err.message}`);
+            }
+          });
+      
+        this.unsubscribe.push(registrationSubscr);
+      } else {
+        // reCAPTCHA validation failed
+        this.hasError = true;
+        alert('reCAPTCHA validation failed. Please try again.');
+      }
+    }).catch(error => {
+      // Handle reCAPTCHA service error
+      this.hasError = true;
+      console.error(error);
+      alert(`reCAPTCHA verification failed: ${error.message}`);
+    });
+    
   }
+
+  // onSubmit() {
+  //   this.verifyRecaptchaToken()
+  //   .pipe(
+  //     concatMap(valid => {
+  //       if (valid) {
+  //         // reCAPTCHA validation successful, proceed with form submission or other actions
+  //         console.log('reCAPTCHA validation successful');
+  //         // Return the result of the submission logic
+  //         this.submit()
+  //         return of(true);
+  //       } else {
+  //         // reCAPTCHA validation failed, display error message or take appropriate action
+  //         console.error('reCAPTCHA validation failed');
+  //         // Return an Observable with false value
+  //         return of(false);
+  //       }
+  //     })
+  //   )
+  //   .subscribe(submissionResult => {
+  //     if (submissionResult) {
+  //       // Form submission successful
+  //       console.log('Form submitted successfully');
+  //     } else {
+  //       // Form submission failed due to reCAPTCHA validation failure
+  //       console.error('Form submission failed due to reCAPTCHA validation failure');
+  //     }
+  //   });
+  // }
   
   handleRecaptchaSuccess(token: any): void {
     console.log('handleRecaptchaSuccess', token);
-    // this.recaptchaResponse = token;
+    this.recaptchaToken = token;
   }
 
   handleRecaptchaError(error: any): void {
@@ -228,19 +266,25 @@ export class RegistrationComponent implements OnInit, OnDestroy {
     this.recaptchaError = error;
   }
 
-  private verifyRecaptchaToken(token: string): void {
-    this.recaptchaService.verifyRecaptcha(token)
-    .subscribe(valid => {
-      if (valid) {
-        // reCAPTCHA validation successful, proceed with form submission or other actions
-        console.log('reCAPTCHA validation successful');
-      } else {
-        // reCAPTCHA validation failed, display error message or take appropriate action
-        console.error('reCAPTCHA validation failed');
-      }
-    });
+  handleRecaptchaReset(): void {
+    // if (this.captchaElem) {
+    //    this.captchaElem.resetCaptcha()
+    // }
   }
+
+  // private verifyRecaptchaToken(): Observable<boolean> {
+  //   return this.recaptchaService.verifyRecaptcha(this.recaptchaToken);
+  // }
   
+  async verifyCaptchaToken(): Promise<boolean> {
+    try {
+      const valid = await this.recaptchaService.verifyRecaptcha(this.recaptchaToken).toPromise();
+      return !!valid;
+    } catch (error) {
+      console.error("reCAPTCHA verification error:", error);
+      return false;
+    }
+  }
   
   
 
