3a1a2eb76d0443d5f1ef7f2bd19b861ef759b894
gracefour11 <76483230+gracefour11@users.noreply.github.com>
2024-04-27 20:11:44 +0800

    remove captcha sec key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -168,7 +168,6 @@ export const environment = {
 
   captcha: {
     siteKey: "6LflWcEpAAAAAKPLs5XTe4575faLtmujHjH6kJn-",
-    secretKey: "6LflWcEpAAAAAA_QXcEzq2ioQCZHhpnYRszFKA4h",
     cloudFunctionUrl: "https://us-central1-quirkandthread-a151e.cloudfunctions.net/verifyRecaptchaOnProd"
   },
 
